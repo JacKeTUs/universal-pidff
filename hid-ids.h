365e585336c60287404184b42639b371eab2b70b
Oleg <oleg@makarenk.ooo>
2024-07-15 04:19:20 +0300

    Added VRS DirectForce Pro ids

--- hid-ids.h
+++ hid-ids.h
@@ -2,11 +2,14 @@
 #ifndef __HID_IDS_H
 #define __HID_IDS_H
 
-#define USB_VENDOR_ID_MOZA		    0x346e
+#define USB_VENDOR_ID_MOZA          0x346e
 #define USB_DEVICE_ID_MOZA_R3       0x0005
 #define USB_DEVICE_ID_MOZA_R5       0x0004
 #define USB_DEVICE_ID_MOZA_R9       0x0002
 #define USB_DEVICE_ID_MOZA_R12      0x0006
 #define USB_DEVICE_ID_MOZA_R16_R21  0x0000
+
+#define USB_VENDOR_ID_VRS           0x0483
+#define USB_DEVICE_ID_VRS_DFP       0xa355
 
 #endif
