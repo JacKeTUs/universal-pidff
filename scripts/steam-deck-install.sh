8b0651edc4f8890e62575e6408b30dd1853f31c8
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2025-07-02 20:08:43 +0200

    Improve Steam Deck installaction script
    - Remove the need to input sudo password multiple times
    - Download only plain PKGBUILD

--- scripts/steam-deck-install.sh
+++ scripts/steam-deck-install.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 ro_status=$(steamos-readonly status)
+install_folder_name="universal-pidff-install"
 
 if [[ $ro_status == "enabled" ]]; then
     echo "Disabling readonly"
@@ -12,36 +13,32 @@ pacman-key --init
 pacman-key --populate archlinux
 pacman-key --populate holo
 
-mkdir universal-pidff-install
-cd universal-pidff-install
+mkdir "$install_folder_name"
+cd "$install_folder_name" || exit 1
 
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/universal-pidff-dkms-git.tar.gz
-tar -xf universal-pidff-dkms-git.tar.gz
+AUR_LINK="https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h="
+curl "${AUR_LINK}universal-pidff-dkms-git" -o PKGBUILD_PIDFF
 
-# to ABSOLUTELY make sure we have acces when running sudo -u deck
-chown -R deck:deck universal-pidff-dkms-git
-chmod 777 universal-pidff-dkms-git
+chown deck:deck .
+chmod 777 .
 
 echo ""
 echo "Don't worry about \"error: command failed to execute correctly\""
 echo ""
 linux=$(pacman -Qsq linux-neptune | grep -e "[0-9]$" | tail -n 1)
-pacman -Syu --noconfirm base-devel fakeroot glibc git \
+pacman -Syu --noconfirm base-devel fakeroot gcc glibc git \
     $linux $linux-headers linux-api-headers
 
-echo ""
-echo "You'll be asked for password again :("
-echo ""
-sleep 2s
+pacman -S --asdeps --noconfirm dkms
 
-cd universal-pidff-dkms-git
-sudo -u deck makepkg -scri --noconfirm
+sudo -u deck makepkg -Cc -p PKGBUILD_PIDFF
+pacman -U --noconfirm universal-pidff-dkms-git-*.tar.zst
 
 echo ""
 echo "Again, don't worry about this ^"
 
-cd ../..
-rm -rf universal-pidff-install
+cd ..
+rm -rf "$install_folder_name"
 
 echo ""
 echo ""
